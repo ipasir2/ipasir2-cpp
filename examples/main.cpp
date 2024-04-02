8f3e012f33be8475675de56733e39005b7142f47
Felix Kutzner <felixkutzner@gmail.com>
2024-04-02 15:00:09 +0300

    Remove model-counter example

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,12 +2,10 @@ void example_01_readme();
 void example_02_trivial_portfolio();
 void example_03_find_backbones();
 void example_04_custom_types();
-void example_05_model_counter();
 
 int main(int, char**) {
   example_01_readme();
   example_02_trivial_portfolio();
   example_03_find_backbones();
   example_04_custom_types();
-  example_05_model_counter();
 }
